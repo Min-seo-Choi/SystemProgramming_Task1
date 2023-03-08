c85ff44c3117a2048fbda646e79ac162c02f271f
Min-seo-Choi <mschoi0814@gmail.com>
2023-03-08 10:55:43 +0900

    setting.ini에 설정된 폴더에 있는 json 확장자만 파일명을 출력한다.

--- Task_1/Task_1.cpp
+++ Task_1/Task_1.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <string>
 #include <windows.h>
 #include <filesystem>
 #include <fstream>
@@ -6,42 +7,53 @@
 
 using namespace std;
 
+string wstringToString(const wstring& wstr);
+
 int main()
 {
-	//ofstream file("setting.ini");
+	//ofstream file("Test_2.json");
 	//if (file.is_open()) {
-.	//	file << "[Section]" << endl;
-	//	file << "DirectoryName=\\JSON" << endl;
-	//	file << "LogLevel=2" << endl;
-	//	file.close();
+	//	//file << "[Section]" << endl;
+	//	//file << "DirectoryName=\\JSON" << endl;
+	//	//file << "LogLevel=2" << endl;
+	//	//file.close();
 	//	cout << "setting.ini file created successfully" << endl;
 	//}
 	//else {
 	//	cerr << "Error: failed to create setting.ini file" << endl;
 	//}
 
-	//bool result = WritePrivateProfileString(L"INFO", L"DiretoryName", L"\\JSON", L"setting.ini");
-
-	//if (result)
-	//{
-	//	wcout << "setting.ini file created successfully" << endl;
-	//}
-
-	//wchar_t dirName[256];
-	//GetPrivateProfileString(L"Section", L"DiretoryName", L" _ ", dirName, 256, L"setting.ini");
-	//wcout << "Directory name: " << dirName << endl;
-
 	WCHAR buffer[MAX_PATH];
-	DWORD result = GetPrivateProfileString(L"INFO", L"DiretoryName", L"", buffer, MAX_PATH, L".\\setting.ini");
+	bool result = GetPrivateProfileString(L"INFO", L"DiretoryName", L"", buffer, MAX_PATH, L".\\setting.ini");
 
-	if (result != 0)
+	if (result)
 	{
-		wcout << buffer << endl;
-	}
-	else
-	{
-		cerr << "Error: Unable to retrieve value from setting.ini" << endl;
-	}
+		string folder_path = wstringToString(buffer);
+		folder_path.insert(0, ".\\");
 
+		// setting.ini에 설정된 폴더에 있는 json 확장자만 파일명을 출력한다.
+		for (const auto& entry : filesystem::directory_iterator(folder_path))
+		{
+			if (entry.path().extension() == ".json")
+			{
+				cout << "File name : " << entry.path().filename() << endl;
+			}
+		}
+
+		// 해당 디렉토리가 없으면 만들건지, 나갈건지(?) 선택
+	}
 	return 0;
 }
+
+string wstringToString(const wstring& wstr) {
+	int size = WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, nullptr, 0, nullptr, nullptr);
+	string str(size - 1, 0);
+	WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, &str[0], size, nullptr, nullptr);
+	return str;
+}
+
+//for (fs::directory_iterator iter(folder_path); iter != fs::directory_iterator(fs::path(folder_path).parent_path().string() + "\\"); ++iter) {
+//	if (iter->path().extension() == ".json") {
+//		std::cout << "File name: " << iter->path().filename() << std::endl;
+//	}
+//}
