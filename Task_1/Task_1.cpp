bb2871826032d77eee8668da17d082a8f210f47a
Min-seo-Choi <Min-seo-Choi@users.noreply.github.com>
2023-03-12 04:04:03 +0900

    프로젝트명 수정 및 파일 추가

--- Task_1/Task_1.cpp
+++ Task_1/Task_1.cpp
@@ -63,7 +63,6 @@ int main()
 		}
 		else
 		{
-			// 코드 푸시 테스트
 			// 해당 디렉토리가 없으면 만들건지, 나갈건지(?) 선택
 			// cout << "Folder does not exist" << endl;
 		}
---
+++ Task_1/WatchJSON.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class WatchJSON
+{
+public:
+
+private:
+};
+
